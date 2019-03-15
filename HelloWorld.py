1d52fc88b681738199dabb3c64b1e8288a3a193a
lykion <34123797+lykion@users.noreply.github.com>
2019-03-15 17:02:33 +0800

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,20 +1,21 @@
 # -*- coding:utf-8 -*-
 import os
 import xlrd
+from log import LOG
 
 string = 'Hello World,I am your best friend'
-print(string)
+LOG().info(string)
 
 path = os.path.join(os.getcwd(), 'API_Case.xlsx')
-print(path)
+LOG().info(path)
 
 workbook = xlrd.open_workbook(path)
 worksheet = workbook.sheets()[0]
-print(worksheet.name)
+LOG().info(worksheet.name)
 
 rows = worksheet.nrows
-print(rows)
-print("================= 开始读取Excel文件 =================")
+LOG().info(rows)
+LOG().info("================= 开始读取Excel文件 =================")
 for i in range(1, rows):
     id = int(worksheet.cell_value(i, 0))
     name = worksheet.cell_value(i, 1)
@@ -22,5 +23,10 @@ for i in range(1, rows):
     url = worksheet.cell_value(i, 3)
     method = worksheet.cell_value(i, 4)
     data = worksheet.cell_value(i, 7)
-    print(id, name, host, url, method, data)
-print("================= Excel文件读取结束 =================")
+    LOG().info(id)
+    LOG().info(name)
+    LOG().info(host)
+    LOG().info(url)
+    LOG().info(method)
+    LOG().info(data)
+LOG().info("================= Excel文件读取结束 =================")
