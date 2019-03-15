659b063d2db38a22e6bbd9c874970d9aea0b7a2f
lykion <34123797+lykion@users.noreply.github.com>
2019-03-15 16:50:10 +0800

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,9 +1,25 @@
 # -*- coding:utf-8 -*-
-import xdrlib
 import os
+import xlrd
 
 string = 'Hello World,I am your best friend'
 print(string)
 
 path = os.path.join(os.getcwd(), 'API_Case.xlsx')
 print(path)
+
+workbook = xlrd.open_workbook(path)
+worksheet = workbook.sheets()[0]
+print(worksheet.name)
+
+rows = worksheet.nrows
+print(rows)
+
+for i in range(1, rows):
+    id = int(worksheet.cell_value(i, 0))
+    name = worksheet.cell_value(i, 1)
+    host = worksheet.cell_value(i, 2)
+    url = worksheet.cell_value(i, 3)
+    method = worksheet.cell_value(i, 4)
+    data = worksheet.cell_value(i, 7)
+    print(id, name, host, url, method, data)
