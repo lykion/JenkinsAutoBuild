aabba5ba928dea31908fdba9a39e4e94b2899212
lykion <34123797+lykion@users.noreply.github.com>
2019-03-15 17:18:26 +0800

    Update log.py

--- log.py
+++ log.py
@@ -1,21 +1,44 @@
-# -*- coding:utf-8 -*-
-import logging
+# -*- coding: utf-8 -*-
+"""日志模块"""
+import os
+import logbook
+from logbook.more import ColorizedStderrHandler
+from functools import wraps
 
-class LOG:
-    def __init__(self):
-        self.logger = logging.getLogger()
-        self.logger.setLevel(logging.INFO)
-
-        self.formatter = logging.Formatter('%(asctime)s -- %(levelname)s: %(message)s')
-
-        self.consle = logging.StreamHandler()
-        self.consle.setLevel(logging.INFO)
-        self.consle.setFormatter(self.formatter)
-        self.logger.addHandler(self.consle)
-
-    def info(self, msg):
-        self.logger.info(msg)
-        self.logger.removeHandler(self.consle)
+check_path = '.'
+LOG_DIR = os.path.join(check_path, 'log')
+file_stream = False
+if not os.path.exists(LOG_DIR):
+    os.makedirs(LOG_DIR)
+    file_stream = True
 
 
-LOG().info('15451212')
+def get_logger(name='HelloWorld', file_log=file_stream, level=''):
+    """ get logger Factory function """
+    logbook.set_datetime_format('local')
+    ColorizedStderrHandler(bubble=False, level=level).push_thread()
+    logbook.TimedRotatingFileHandler(os.path.join(LOG_DIR, '%s.log' % name), date_format='%Y-%m-%d-%H', bubble=True,
+        encoding='utf-8').push_thread()
+    return logbook.Logger(name)
+
+
+LOG = get_logger(file_log=file_stream, level='INFO')
+
+
+def logger(param):
+    """ fcuntion from logger meta """
+
+    def wrap(function):
+        """ logger wrapper """
+
+        @wraps(function)
+        def _wrap(*args, **kwargs):
+            """ wrap tool """
+            LOG.info("当前模块 {}".format(param))
+            LOG.info("全部args参数参数信息 , {}".format(str(args)))
+            LOG.info("全部kwargs参数信息 , {}".format(str(kwargs)))
+            return function(*args, **kwargs)
+
+        return _wrap
+
+    return wrap
